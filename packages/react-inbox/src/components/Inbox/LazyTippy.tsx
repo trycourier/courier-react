532c244fcecfd8ad96a8b3044ad508b01e053bfd
riley napier <riley@trycourer.com>
2021-03-22 12:15:32 -0700

    dont unmount on hidden

--- packages/react-inbox/src/components/Inbox/LazyTippy.tsx
+++ packages/react-inbox/src/components/Inbox/LazyTippy.tsx
@@ -1,6 +1,9 @@
 import React, { forwardRef, useState } from "react";
 import Tippy, { TippyProps } from "@tippyjs/react";
 
+/*
+found here: https://gist.github.com/atomiks/520f4b0c7b537202a23a3059d4eec908
+*/
 const LazyTippy: React.FunctionComponent<TippyProps> = forwardRef(
   (props, ref) => {
     const [mounted, setMounted] = useState(false);
