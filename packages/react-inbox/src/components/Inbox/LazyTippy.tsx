e49702609eaffc011d6eebbe07cfb2ad5d63b14b
riley napier <riley@trycourer.com>
2021-03-22 11:46:33 -0700

    lazy tippy

---
+++ packages/react-inbox/src/components/Inbox/LazyTippy.tsx
@@ -0,0 +1,24 @@
+import React, { forwardRef, useState } from "react";
+import Tippy, { TippyProps } from "@tippyjs/react";
+
+const LazyTippy: React.FunctionComponent<TippyProps> = forwardRef(
+  (props, ref) => {
+    const [mounted, setMounted] = useState(false);
+
+    const lazyPlugin = {
+      fn: () => ({
+        onMount: () => setMounted(true),
+        onHidden: () => setMounted(false),
+      }),
+    };
+
+    const computedProps = { ...props };
+
+    computedProps.plugins = [lazyPlugin, ...(props.plugins || [])];
+    computedProps.content = mounted ? props.content : "";
+
+    return <Tippy {...computedProps} ref={ref} />;
+  }
+);
+
+export default LazyTippy;
---
+++ packages/react-inbox/src/components/Messages/loading.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import { Container, Contents } from "../Message/styled";
+
+const MessageSkeleton: React.FunctionComponent = () => {
+  return (
+    <Container>
+      <Skeleton
+        circle={true}
+        height={35}
+        width={35}
+        style={{ marginRight: 12 }}
+      />
+      <Contents style={{ width: "100%" }}>
+        <Skeleton count={2} />
+      </Contents>
+      <div style={{ width: "100px", marginLeft: 12 }}>
+        <Skeleton count={1} height={20} style={{ marginTop: 8 }} />
+      </div>
+    </Container>
+  );
+};
+
+const Loading: React.FunctionComponent = () => {
+  return (
+    <>
+      <MessageSkeleton />
+      <MessageSkeleton />
+    </>
+  );
+};
+
+export default Loading;
