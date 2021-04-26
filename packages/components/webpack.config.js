f2f9e2d8e92d2a6d885705d1955eb6856905dc60
Riley <riley@trycourier.com>
2021-04-26 12:48:15 -0700

    Version25 (#59)
    * fix css
    
    * save
    
    Co-authored-by: riley napier <riley@trycourer.com>

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -40,10 +40,6 @@ module.exports = (env, argv) => {
             loader: "babel-loader",
           },
         },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
       ],
     },
     devServer: {
