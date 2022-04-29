b899261c41faa022977a4dd3075cfef08a4b0420
Riley <rileylnapier@gmail.com>
2022-04-29 10:16:02 -0700

    Riley fix auth (#184)
    * asve
    
    * save
    
    * save

--- packages/client-graphql/src/client.ts
+++ packages/client-graphql/src/client.ts
@@ -24,7 +24,7 @@ export const createCourierClient = (
   if ("authorization" in params) {
     headers = {
       "x-courier-client-key": params.clientKey,
-      authorization: params.authorization,
+      authorization: `Bearer ${params.authorization}`,
     } as CourierJWTHeaders;
   } else {
     const {
