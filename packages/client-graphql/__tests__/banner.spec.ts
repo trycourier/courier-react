6481ab9fccd9f2e2925e71c224ea21659685cf35
Riley <rileylnapier@gmail.com>
2022-05-11 16:53:46 -0700

    save (#189)

--- packages/client-graphql/__tests__/banner.spec.ts
+++ packages/client-graphql/__tests__/banner.spec.ts
@@ -20,7 +20,7 @@ describe("banner", () => {
 
     const thisCall = fetchMock.mock.calls[0][1];
     expect(thisCall.body).toMatchInlineSnapshot(
-      `"{\\"query\\":\\"query GetBanners($params: BannerParamsInput, $limit: Int = 10, $after: String) {\\\\n  banners(params: $params, limit: $limit, after: $after) {\\\\n    totalCount\\\\n    pageInfo {\\\\n      startCursor\\\\n      hasNextPage\\\\n      __typename\\\\n    }\\\\n    nodes {\\\\n      id\\\\n      userId\\\\n      messageId\\\\n      created\\\\n      tags\\\\n      content {\\\\n        title\\\\n        body\\\\n        blocks {\\\\n          ... on TextBlock {\\\\n            type\\\\n            text\\\\n            __typename\\\\n          }\\\\n          ... on ActionBlock {\\\\n            type\\\\n            text\\\\n            url\\\\n            __typename\\\\n          }\\\\n          __typename\\\\n        }\\\\n        data\\\\n        __typename\\\\n      }\\\\n      __typename\\\\n    }\\\\n    __typename\\\\n  }\\\\n}\\\\n\\",\\"operationName\\":\\"GetBanners\\",\\"variables\\":{}}"`
+      `"{\\"query\\":\\"query GetBanners($params: BannerParamsInput, $limit: Int = 10, $after: String) {\\\\n  banners(params: $params, limit: $limit, after: $after) {\\\\n    totalCount\\\\n    pageInfo {\\\\n      startCursor\\\\n      hasNextPage\\\\n      __typename\\\\n    }\\\\n    nodes {\\\\n      id\\\\n      userId\\\\n      messageId\\\\n      created\\\\n      tags\\\\n      content {\\\\n        title\\\\n        body\\\\n        blocks {\\\\n          ... on TextBlock {\\\\n            type\\\\n            text\\\\n            __typename\\\\n          }\\\\n          ... on ActionBlock {\\\\n            type\\\\n            text\\\\n            url\\\\n            __typename\\\\n          }\\\\n          __typename\\\\n        }\\\\n        data\\\\n        __typename\\\\n      }\\\\n      __typename\\\\n    }\\\\n    __typename\\\\n  }\\\\n}\\\\n\\",\\"operationName\\":\\"GetBanners\\",\\"variables\\":{\\"params\\":{}}}"`
     );
     expect(thisCall.headers).toEqual({
       "content-type": "application/json",
@@ -28,6 +28,23 @@ describe("banner", () => {
       "x-courier-user-id": "USER_ID",
     });
     expect(thisCall.method).toBe("POST");
+  });
+
+  test("creates query correctly with limit", async () => {
+    fetchMock.mockImplementation(() => Promise.resolve([]));
+    const bannerApi = Banner({
+      clientKey: "CLIENT_KEY",
+      userId: "USER_ID",
+    });
+
+    await bannerApi.getBanners({
+      limit: 100,
+    });
+
+    const thisCall = fetchMock.mock.calls[0][1];
+    expect(thisCall.body).toMatchInlineSnapshot(
+      `"{\\"query\\":\\"query GetBanners($params: BannerParamsInput, $limit: Int = 10, $after: String) {\\\\n  banners(params: $params, limit: $limit, after: $after) {\\\\n    totalCount\\\\n    pageInfo {\\\\n      startCursor\\\\n      hasNextPage\\\\n      __typename\\\\n    }\\\\n    nodes {\\\\n      id\\\\n      userId\\\\n      messageId\\\\n      created\\\\n      tags\\\\n      content {\\\\n        title\\\\n        body\\\\n        blocks {\\\\n          ... on TextBlock {\\\\n            type\\\\n            text\\\\n            __typename\\\\n          }\\\\n          ... on ActionBlock {\\\\n            type\\\\n            text\\\\n            url\\\\n            __typename\\\\n          }\\\\n          __typename\\\\n        }\\\\n        data\\\\n        __typename\\\\n      }\\\\n      __typename\\\\n    }\\\\n    __typename\\\\n  }\\\\n}\\\\n\\",\\"operationName\\":\\"GetBanners\\",\\"variables\\":{\\"limit\\":100,\\"params\\":{}}}"`
+    );
   });
 
   test("creates query correctly with jwt", async () => {
