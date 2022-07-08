e0795a0e0ceb6ca545dcd690e43d40a72205ddc3
Christian Glassiognon <christian@heyglassy.co>
2022-07-08 05:08:58 -0700

    fix test

--- packages/react-preferences/src/components/DeliveryChannels.tsx
+++ packages/react-preferences/src/components/DeliveryChannels.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { PreferenceItemComponentFn } from "../types";
 
 // TODO
-export const ChannelPreferences: React.FC<{}> = () => <></>;
+export const ChannelPreferences: React.FC = () => <></>;
