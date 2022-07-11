fae9e75999595c9b283e2054c07f63c83d2e21ae
Riley <rileylnapier@gmail.com>
2022-07-11 14:09:04 -0700

    save

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -1,29 +1,5 @@
-module.exports = {
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
-  roots: ["<rootDir>"],
+/* eslint-disable @typescript-eslint/no-var-requires */
+const jestConfigBase = require("../../jest.config.base");
+const babelConfig = require("./babel.config.js");
 
-  globals: {
-    "ts-jest": {
-      babelConfig: require("./babel.config.js"),
-    },
-  },
-
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
-  transform: {
-    "\\.(ts|tsx)$": "ts-jest",
-  },
-
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-
-  // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
-  moduleNameMapper: {
-    "~(.*)$": "<rootDir>/src/$1",
-  },
-  testPathIgnorePatterns: ["/node_modules/"],
-};
+module.exports = jestConfigBase(babelConfig);
