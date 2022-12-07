2ff4f44755958ca7164d0827df454b9f3aba745c
Christian Glassiognon <christian@heyglassy.co>
2022-12-07 15:04:36 -0800

    moved to styled components

--- packages/components/src/components/PreferencePage.tsx
+++ packages/components/src/components/PreferencePage.tsx
@@ -1,39 +1,40 @@
 import { Footer, Header, PreferencesV4 } from "@trycourier/react-preferences";
 import React from "react";
+import styled from "styled-components";
+
+const PreferencePageContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PreferencesV4Container = styled.div`
+  width: 50%;
+  flex-direction: column;
+  background: rgba(255, 255, 255, 0.2);
+  display: flex;
+  overflow: auto;
+`;
+
+const HeaderContainer = styled.div`
+  width: 50%;
+`;
 
 const PreferencePage: React.FunctionComponent<{ draft?: boolean }> = ({
   draft = false,
 }) => {
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          width: "50%",
-        }}
-      >
+    <PreferencePageContainer>
+      <HeaderContainer>
         <Header />
-      </div>
-      <div
-        style={{
-          overflow: "auto",
-          display: "flex",
-          flexDirection: "column",
-          background: "rgba(255, 255, 255, 0.2)",
-          width: "50%",
-        }}
-      >
+      </HeaderContainer>
+      <PreferencesV4Container>
         <PreferencesV4 draft={draft} />
-      </div>
+      </PreferencesV4Container>
       <Footer />
-    </div>
+    </PreferencePageContainer>
   );
 };
 
