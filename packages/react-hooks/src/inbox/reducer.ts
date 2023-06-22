6cabfad7b2cd2968a47a990b9cd203c666410fca
Riley <rileylnapier@gmail.com>
2023-06-22 12:39:12 +0100

    chore(save): qUICK SAVE

--- packages/react-hooks/src/inbox/reducer.ts
+++ packages/react-hooks/src/inbox/reducer.ts
@@ -163,7 +163,6 @@ export default (state: IInbox = initialState, action?: InboxAction): IInbox => {
       return {
         ...state,
         isLoading: false,
-        lastMessagesFetched: new Date().getTime(),
         messages: newMessages,
         pinned: action.payload.appendMessages
           ? state.pinned
