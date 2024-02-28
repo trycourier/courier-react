64fba8a815e66782181d99ae392f765ed051e88e
paige12345 <93237887+paige12345@users.noreply.github.com>
2024-02-28 17:01:01 +0800

    fix(export PreferenceSections): integrate PreferenceSections

--- packages/react-preferences/src/components/index.ts
+++ packages/react-preferences/src/components/index.ts
@@ -1,6 +1,6 @@
 export { UnsubscribePage } from "./UnsubscribePage";
 export { PreferenceTemplate } from "./PreferenceTemplate";
 export { PreferenceList } from "./PreferenceList";
-export { PreferencesV4 } from "./PreferencesV4";
+export { PreferencesV4, PreferenceSections } from "./PreferencesV4";
 export { Footer } from "./Footer";
 export { Header } from "./Header";
