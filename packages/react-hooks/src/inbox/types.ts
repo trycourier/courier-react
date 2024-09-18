ad3744dcbccf9b43d72b9322bd30eb3e9ef93710
Riley Love <rileylnapier@gmail.com>
2024-09-18 10:27:12 -0700

    fix(fix referfch after archive): fix refetch after archiving the last message

--- packages/react-hooks/src/inbox/types.ts
+++ packages/react-hooks/src/inbox/types.ts
@@ -2,7 +2,6 @@ import { IGetInboxMessagesParams } from "@trycourier/client-graphql";
 import { Brand, IInboxMessagePreview } from "@trycourier/core";
 import { OnEvent } from "@trycourier/react-provider";
 export interface IInbox<T = IInboxMessagePreview> {
-  tenantId?: string;
   brand?: Brand;
   from?: number;
   isLoading?: boolean;
@@ -11,9 +10,11 @@ export interface IInbox<T = IInboxMessagePreview> {
   lastMessagesFetched?: number;
   messages?: Array<T>;
   onEvent?: OnEvent;
-  searchParams?: IGetInboxMessagesParams;
   pinned?: Array<T>;
+  recentlyArchiveMessageIds?: Array<string>;
+  searchParams?: IGetInboxMessagesParams;
   startCursor?: string;
+  tenantId?: string;
   unreadMessageCount?: number;
   view?: string | "preferences";
 }
