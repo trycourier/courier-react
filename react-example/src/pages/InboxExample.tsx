d96b1f2af899594134af96313fbf8b48080be531
Mike Miller <mike@mikemiller.design>
2026-01-28 11:38:49 -0800

    Inbox example

---
+++ react-example/src/pages/InboxExample.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Inbox } from "@trycourier/react-inbox";
+
+const InboxExample: React.FC = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <Inbox />
+    </div>
+  );
+};
+
+export default InboxExample;
